75b6fba8df7ac243bc2ab7149d66d4692987769e
Martin Eek Gerhardsen <martiege@stud.ntnu.no>
2019-01-30 10:20:29 +0100

    changes...

--- Exercise3/udpclient/lib/udp_client.ex
+++ Exercise3/udpclient/lib/udp_client.ex
@@ -25,19 +25,34 @@ defmodule UDPClient do
     # sendSock.sendTo(message, addr)
   end
 
-  def reciever() do
-    byte[1024] buffer
-    InternetAddress fromWho
-    recvSock = new Socket(udp)
-    recvSock.bind(addr) # same addr as sender
-    loop
-      buffer.clear
+  def reciever(port, address) do
+    {:ok, recvSock} = :gen_udp.open(port, [{:reuseaddr, true}, {:ip, address}])
+    # bind to addr?
+    receive(recvSock) 
+
+
+    #byte[1024] buffer
+    #InternetAddress fromWho
+    #recvSock = new Socket(udp)
+    #recvSock.bind(addr) # same addr as sender
+    #loop
+      #buffer.clear
 
       # fromWho will be modified by ref here. Or it's a return value. Depends
-      recvSock.receiveFrom(buffer, ref fromWho)
-      if (fromWho.IP != localIP) # check we are not receiving from ourselves
+      #recvSock.receiveFrom(buffer, ref fromWho)
+      #if (fromWho.IP != localIP) # check we are not receiving from ourselves
         # do stuff with buffer
-      end
+      #end
+  end
+
+  def reciever(recvSock) do
+    {ok:, {address, port, packet}} = :gen_udp.recv(recvSock, 1024)
+    reciever(address, port, packet)
+    reciever(recvSock)
+  end
+
+  def reciever(address, port, packet) when address != localIP do
+    IO.puts "This packet was sent: #{packet}"
   end
 
 end
