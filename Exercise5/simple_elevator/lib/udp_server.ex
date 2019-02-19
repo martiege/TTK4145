d2e4037fe1619fd61f4a4c2e8ad3a52adec7fac9
Martin Eek Gerhardsen <martiege@stud.ntnu.no>
2019-02-19 11:46:12 +0100

    yay, can use GenServer to handle UDP messages

--- Exercise5/simple_elevator/lib/udp_server.ex
+++ Exercise5/simple_elevator/lib/udp_server.ex
@@ -1,44 +1,77 @@
 defmodule UDPServer do
-  #use GenServer
-
-  def start_link(port, opts) do
-
-  end
-
-  def init(port, opts) do
-    {:ok, socket} = :gen_udp.open(port, opts)
-
-    # look for other elevators on system?
-    {:ok, socket}
-  end
-
-
-
-  def start_link(port) do
-    GenServer.start_link(__MODULE__, port)
-  end
-
-  def init(port) do
-    :gen_udp.open(port, [:binary, active: true])
-  end
-
-  def handle_info({:udp, _socket, _address, _port, data}, socket) do
-    handle_packet(data, socket)
-  end
-
-
-  defp handle_packet("quit\n", socket) do
-    IO.puts("Received: quit")
-    :gen_udp.close(socket)
-
-    {:stop, :normal, nil}
-  end
-
-  defp handle_packet(data, socket) do
-    IO.puts("Received: #{String.trim data}")
-    {:noreply, socket}
-  end
 
 end
 
-#{:ok, _pid} = Supervisor.start_link([{UDP, 2052}], strategy: :one_for_one)
+defmodule UDPServer.Supervisor do
+  use Supervisor
+
+
+end
+
+defmodule UDPServer.Receiver do
+  @receiver_port    60000
+  @receiver_options [:list, active: true]
+  @receiver_timer   1000
+  # specialized GenServer to handle incoming data
+  # from any other elevator on the network
+
+  use GenServer
+
+  def start_link(port \\ @receiver_port, opts \\ @receiver_options) do
+    GenServer.start_link(__MODULE__, {port, opts}, name: __MODULE__)
+  end
+
+
+  def init({port, opts}) do
+    :gen_udp.open(port, opts)
+  end
+
+
+  def handle_info({:udp, _socket, _address, _port, data}, receiver_socket) do
+    IO.puts("Received packet: #{data}")
+
+    {:noreply, receiver_socket}
+  end
+
+
+  def handle_info(other, receiver_socket) do
+    IO.puts("Huh? #{other}")
+
+    {:noreply, receiver_socket}
+  end
+
+  # function to look for existing elevator network?
+  # def udp_server_receiver_init(receiver_socket) do
+  #   IO.puts("UDP receiver init")
+  #   Process.sleep(1000)
+  #
+  #   udp_server_receiver(receiver_socket)
+  # end
+  #
+  # def udp_server_receiver(receiver_socket) do
+  #   #Process.sleep(1000)
+  #   IO.puts("UDP receiver main")
+  #
+  #   {:ok, received} = :gen_udp.recv(receiver_socket, 0, @receiver_timer)
+  #
+  #
+  #   udp_server_receiver(receiver_socket)
+  # end
+  #
+  # def handle_receive({address, port, packet}) do
+  #
+  # end
+  #
+  # def handle_receive({})
+
+end
+
+# defmodule UDPServer.Transmitter do
+#   # GenServer to handle outgoing data,
+#   # or incoming messages from the rest of the system
+#   # and appropriatly send these to the rest of the network
+#
+#   use GenServer
+#
+#
+# end
