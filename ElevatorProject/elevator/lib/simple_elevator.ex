a74c0a2567b6698ba760c288c0467e02c31ea4b5
kennyhn <kennyhn@stud.ntnu.no>
2019-03-11 19:33:49 +0100

    continuing work on syncing states

--- ElevatorProject/elevator/lib/simple_elevator.ex
+++ ElevatorProject/elevator/lib/simple_elevator.ex
@@ -32,86 +32,109 @@ defmodule SimpleElevator do
     GenStateMachine.start_link(__MODULE__, {state, data}, [name: __MODULE__])
   end
 
-  # Cast functions
-
+  # Floor
+  # cast
   def handle_event(:cast, {:set_floor, floor}, state, data) do
     state = Map.replace!(state, :floor, floor)
     {:next_state, state, data}
   end
 
+  # call
+  def handle_event({:call, from}, :get_floor, state, data) do
+    {:next_state, state, data, [{:reply, from, state[:floor]}]}
+  end
+
+  # Requests 
+  # command 
+  # cast 
   def handle_event(:cast, {:set_command, floor, new_state}, state, data) do
     # TODO: simplify
     state = Map.replace!(state, :command, List.replace_at(state[:command], floor, new_state))
     {:next_state, state, data}
   end
 
+  # call
+  def handle_event({:call, from}, {:get_command, floor}, state, data) do
+    {:next_state, state, data, [{:reply, from, Enum.at(state[:command], floor)}]}
+  end
+
+  # call up
+  # cast
   def handle_event(:cast, {:set_call_up, floor, new_state}, state, data) do
     state = Map.replace!(state, :call_up, List.replace_at(state[:call_up], floor, new_state))
     {:next_state, state, data}
   end
 
+  # call
+  def handle_event({:call, from}, {:get_call_up, floor}, state, data) do
+    {:next_state, state, data, [{:reply, from, Enum.at(state[:call_up], floor)}]}
+  end
+
+  # call down
+  # cast
   def handle_event(:cast, {:set_call_down, floor, new_state}, state, data) do
     state = Map.replace!(state, :call_down, List.replace_at(state[:call_down], floor, new_state))
     {:next_state, state, data}
   end
 
+  # call
+  def handle_event({:call, from}, {:get_call_down, floor}, state, data) do
+    {:next_state, state, data, [{:reply, from, Enum.at(state[:call_down], floor)}]}
+  end
+
+  # direction
+  # cast
   def handle_event(:cast, {:set_dir, dir}, state, data) do
     state = Map.replace!(state, :dir, dir)
     {:next_state, state, data}
   end
 
+  # call
+  def handle_event({:call, from}, :get_dir, state, data) do
+    {:next_state, state, data, [{:reply, from, state[:dir]}]}
+  end
+
+  # behaviour
+  # cast
   def handle_event(:cast, {:set_behaviour, behaviour}, state, data) do
     state = Map.replace!(state, :behaviour, behaviour)
     {:next_state, state, data}
   end
 
+  # call
+  def handle_event({:call, from}, :get_behaviour, state, data) do
+    {:next_state, state, data, [{:reply, from, state[:behaviour]}]}
+  end
+
+  # door 
+  # cast
   def handle_event(:cast, {:set_door, door_state}, state, data) do
     state = Map.replace!(state, :door, door_state)
     {:next_state, state, data}
   end
 
-  def handle_event(:cast, :share_state, state, data) do
-    IO.puts "Start sharing state..."
-
-    GenServer.multi_call([Node.self() | Node.list()], SimpleElevator, :)
-
-    {:next_state, state, data}
-  end
-
-  # Call functions
-
-  def handle_event({:call, from}, :get_floor, state, data) do
-    {:next_state, state, data, [{:reply, from, state[:floor]}]}
-  end
-
-  def handle_event({:call, from}, {:get_command, floor}, state, data) do
-    {:next_state, state, data, [{:reply, from, Enum.at(state[:command], floor)}]}
-  end
-
-  def handle_event({:call, from}, {:get_call_up, floor}, state, data) do
-    {:next_state, state, data, [{:reply, from, Enum.at(state[:call_up], floor)}]}
-  end
-
-  def handle_event({:call, from}, {:get_call_down, floor}, state, data) do
-    {:next_state, state, data, [{:reply, from, Enum.at(state[:call_down], floor)}]}
-  end
-
-  def handle_event({:call, from}, :get_dir, state, data) do
-    {:next_state, state, data, [{:reply, from, state[:dir]}]}
-  end
-
-  def handle_event({:call, from}, :get_behaviour, state, data) do
-    {:next_state, state, data, [{:reply, from, state[:behaviour]}]}
-  end
-
+  # call
   def handle_event({:call, from}, :get_door, state, data) do
     {:next_state, state, data, [{:reply, from, state[:door]}]}
   end
 
+  # start sending local state
+  def handle_event(:cast, :share_state, state, data) do
+    IO.puts "Start sharing state..."
+
+    # self-call as well, might be useful in self-checks
+    # keep ghost state on local machine of itself as well
+    GenServer.multi_call([Node.self() | Node.list()], SimpleElevator, :sync_state, state)
+
+    {:next_state, state, data}
+  end
+
+  # sync ghost state from other nodes (or itself)
   def handle_event({:call, from}, {:get_other_state, other_state}, state, data) do
     IO.puts "Getting another state..."
+    IO.inspect(from)
 
-
+    {:next_state, state, data, [{:reply, from, :ack}]}
   end
 
   # use Agent
